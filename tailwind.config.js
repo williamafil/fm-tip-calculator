3c5a003f87a07553388c82e1ef49e50afd20474f
William <will2b@gmail.com>
2021-08-04 03:04:45 +0800

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          DEFAULT: "hsl(172, 67%, 45%)",
+          600: "hsl(183, 100%, 15%)",
+          500: "hsl(186, 14%, 43%)",
+          400: "hsl(184, 14%, 56%)",
+          300: "hsl(185, 41%, 84%)",
+          200: "hsl(189, 41%, 97%)",
+          100: "hsl(0, 0%, 100%)",
+        },
+      },
+      fontFamily: {
+        "space-mono": ["Space Mono", "serif"],
+      },
+    },
   },
   variants: {
     extend: {},
