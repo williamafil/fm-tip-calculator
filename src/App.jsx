9d6ec7a62c2f0cc47c515660e4bf7bf634068fbc
William <will2b@gmail.com>
2021-08-04 12:41:46 +0800

    rwd layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,100 @@
 import React, { useState } from "react";
 import "./assets/css/App.css";
+// import Logo from "./assets/images/logo.svg";
+
+function getImageUrl(name) {
+  return new URL(`./assets/images/${name}`, import.meta.url).href;
+}
 
 function App() {
-  return <div className="container bg-cyan-300">hefiehih</div>;
+  return (
+    <div className="container mx-auto">
+      <header className="flex justify-center my-12">
+        <img src={getImageUrl("logo.svg")} />
+      </header>
+
+      <main className="bg-white rounded-t-3xl md:rounded-b-3xl w-full p-8 grid md:gap-6 md:grid-flow-row md:grid-cols-2">
+        <aside className="">
+          <section class="mb-8">
+            <label className="label">Bill</label>
+            <div className="input-wrapper relative">
+              <img
+                className="absolute top-1/2 bottom-1/2 my-auto left-5"
+                src={getImageUrl("icon-dollar.svg")}
+              />
+              <input
+                type="number"
+                step="0.01"
+                className="bill-input"
+                placeholder="0"
+              />
+            </div>
+          </section>
+
+          <section className="mb-8">
+            <label className="label">Select Tip %</label>
+            <div className="grid gap-4 grid-flow-row grid-cols-2">
+              <button type="button" value="5%" className="tip-btn">
+                5%
+              </button>
+              <button type="button" value="10%" className="tip-btn">
+                10%
+              </button>
+              <button type="button" value="15%" className="tip-btn">
+                15%
+              </button>
+              <button type="button" value="25%" className="tip-btn">
+                25%
+              </button>
+              <button type="button" value="50%" className="tip-btn">
+                50%
+              </button>
+              <input type="number" className="tip-input" placeholder="Custom" />
+            </div>
+          </section>
+
+          <section className="mb-8 md:mb-0">
+            <label className="label">Number of People</label>
+            <div className="input-wrapper relative">
+              <img
+                className="absolute top-1/2 bottom-1/2 my-auto left-5"
+                src={getImageUrl("icon-person.svg")}
+              />
+              <input type="number" className="people-input" placeholder="1" />
+            </div>
+          </section>
+        </aside>
+
+        <section className="rounded-xl bg-cyan-600 p-6 text-white h-full flex flex-col justify-between">
+          <div>
+            <article class="mb-6 flex justify-between">
+              <div>
+                <label className="font-bold">Tip Amount</label>
+                <p className="text-cyan-400 text-sm">/ person</p>
+              </div>
+
+              <h2 className="text-cyan text-3xl font-bold">$4.27</h2>
+            </article>
+
+            <article class="mb-6 flex justify-between">
+              <div>
+                <label className="font-bold">Total</label>
+                <p className="text-cyan-400 text-sm">/ person</p>
+              </div>
+
+              <h2 className="text-cyan text-3xl font-bold">$32.79</h2>
+            </article>
+          </div>
+          <button
+            type="button"
+            className="uppercase text-center bg-cyan w-full py-3 rounded text-cyan-600 font-bold text-xl"
+          >
+            Reset
+          </button>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -10,13 +12,26 @@ module.exports = {
           600: "hsl(183, 100%, 15%)",
           500: "hsl(186, 14%, 43%)",
           400: "hsl(184, 14%, 56%)",
+          350: "#9FE8DF",
           300: "hsl(185, 41%, 84%)",
-          200: "hsl(189, 41%, 97%)",
-          100: "hsl(0, 0%, 100%)",
+          200: "hsl(189, 41%, 97%)", // #F3F9FA
+          50: "hsl(0, 0%, 100%)",
         },
       },
       fontFamily: {
         "space-mono": ["Space Mono", "serif"],
+      },
+      outline: {
+        cyan: ["1px solid hsl(172, 67%, 45%)", "1px"],
+      },
+      ringColor: {
+        red: colors.red,
+        cyan: colors.cyan,
+      },
+      ringWidth: {
+        DEFAULT: "2px",
+        6: "6px",
+        10: "10px",
       },
     },
   },
